a5599569024a42c80cbeb7c2099d0c7be8703ae7
nanoGonzales <luis.gonzales@culqi.com>
2016-09-16 17:11:47 -0500

    Commit1 - Ejemplos culqi

---
+++ EjemplosCulqi/EjemplosCulqi/Controllers/TokenController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EjemplosCulqi.Controllers
+{
+    public class TokenController : Controller
+    {
+        // GET: Token
+        public ActionResult NuevoToken()
+        {
+            return View();
+        }
+    }
+}
